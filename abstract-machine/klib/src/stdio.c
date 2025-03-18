a49fc4fe201bdc561fd334971de41cdb53e9759a
Minus7 <1511148197@qq.com>
2025-03-18 17:42:29 +0800

    NJU-ProjectN/abstract-machine ics2024 initialized
    NJU-ProjectN/abstract-machine a7b830fedd86505e8accba8f4b928084542c841a feat: riscv64 linux support

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,7 +2,7 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
-//#include<stdio.h>
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
@@ -10,59 +10,11 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  int d;
-  char *s;
- const char *ptr=fmt;
-  while(*ptr){
-    if(*ptr=='%'){
-        ptr++;
-        switch(*ptr){
-        case 'd': d=va_arg(ap,int);
-                  if(d<0){
-                   *out++='-';
-                    d=-d;
-                  }
-                 char buf[12];
-                 int i=0;
-                 if(d==0) *out++='0';
-                 else {
-                    while(d>0){
-                    buf[i++]='0'+d%10;
-                    d=d/10;
-                    }
-                    while(i>0){
-                    *out++=buf[--i];
-                    }
-                 }
-                  ptr++;
-                  break;
-        case 's': s=va_arg(ap,char *);
-                  while(*s){
-                  *out++=*s++;
-                  }
-                  ptr++;
-                  break;
-        default:*out++='%';
-                *out++=*ptr++;
-                break;
-     }
-    }
-    else{
-    *out++=*ptr++;
-    }
-  }
-  *out='\0';
-return ptr-fmt;
+  panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  int n=0;
-//  char *p;
-  va_start(ap,fmt);
-  n=vsprintf(out,fmt,ap);
-  va_end(ap);
-  return n;
+  panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,118 +5,43 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-    const char *p=s;
-    while(*p){
-    p++;
-    }
-    return p-s;
+  panic("Not implemented");
 }
 
 char *strcpy(char *dst, const char *src) {
-     char *p=dst; 
-    while (*src){
-      *p=*src;
-      src++;
-      p++;
-     }
-     *p='\0';
-     return dst;
+  panic("Not implemented");
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-     char *p=dst; 
-     while (n>0){
-      *p=*src;
-      src++;
-      p++;
-      n--;
-     }
-     *p='\0';
-     return dst;
+  panic("Not implemented");
 }
-
 
 char *strcat(char *dst, const char *src) {
-char *p=dst;
-while(*p){
-p++;}
-while(*src){
-   *p=*src;
-   p++;
-   src++;
-}
-*p='\0';
-return dst;
+  panic("Not implemented");
 }
 
 int strcmp(const char *s1, const char *s2) {
-while(*s1){
-if(*s1!=*s2) return *s1-*s2;
-s1++;
-s2++;
-}
-return 0;
+  panic("Not implemented");
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-while(n>0){
-    if(*s1++!=*s2++) return (*s1)-(*s2);
-    n--;
-} 
-return 0;
+  panic("Not implemented");
 }
 
 void *memset(void *s, int c, size_t n) {
-unsigned char *p =s;
-while(n){
-*p++=(unsigned char)c;
-n--;
-}
-return s;
+  panic("Not implemented");
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-unsigned char *d = dst;
-    const unsigned char *s = src;
-    if (d == s || n == 0) {
-        // 源和目标相同，或 n 为 0，不需要移动
-        return dst;
-    }
-
-    if (d < s || d >= s + n) {
-        // 内存区域没有重叠，可以从头到尾正常复制
-        while (n--) {
-            *d++ = *s++;
-        }
-    } else {
-        // 内存区域有重叠，从尾到头复制
-        d += n;
-        s += n;
-        while (n--) {
-            *(--d) = *(--s);
-        }
-    }
-
-    return dst;
+  panic("Not implemented");
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-   const  unsigned char*ptr_in=in;
-    unsigned char*ptr_out=out;
-    while(n--){
-    *ptr_out++=*ptr_in++;
-    }
-    return out;
+  panic("Not implemented");
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-const unsigned char*ptr1=s1;
-const unsigned char*ptr2=s2;
-while(n--){
-if(*ptr1++!=*ptr2++) return *ptr1-*ptr2;
+  panic("Not implemented");
 }
-return 0;
-}
-
 
 #endif
